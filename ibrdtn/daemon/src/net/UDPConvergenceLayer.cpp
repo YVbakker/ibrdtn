d9497b0c82d10f7ffb9c41e06f8cfa2aada3a675
Yael Bakker <yvbakker@Yaels-MacBook-Air.local>
2020-10-12 17:18:41 +0200

    add case stubs to handle PASS and SKIP

--- ibrdtn/daemon/src/net/UDPConvergenceLayer.cpp
+++ ibrdtn/daemon/src/net/UDPConvergenceLayer.cpp
@@ -48,7 +48,6 @@
 #include <list>
 #include <vector>
 
-
 using namespace dtn::data;
 
 namespace dtn
@@ -58,7 +57,7 @@ namespace dtn
 		const int UDPConvergenceLayer::DEFAULT_PORT = 4556;
 
 		UDPConvergenceLayer::UDPConvergenceLayer(ibrcommon::vinterface net, int port, dtn::data::Length mtu)
-		 : _net(net), _port(port), m_maxmsgsize(mtu), _running(false), _stats_in(0), _stats_out(0)
+			: _net(net), _port(port), m_maxmsgsize(mtu), _running(false), _stats_in(0), _stats_out(0)
 		{
 		}
 
@@ -93,69 +92,82 @@ namespace dtn
 			return dtn::core::Node::CONN_UDPIP;
 		}
 
-		void UDPConvergenceLayer::onUpdateBeacon(const ibrcommon::vinterface &iface, DiscoveryBeacon &announcement) noexcept (false)
+		void UDPConvergenceLayer::onUpdateBeacon(const ibrcommon::vinterface &iface, DiscoveryBeacon &announcement) noexcept(false)
 		{
 			// announce port only if we are bound to any interface
-			if (_net.isAny()) {
+			if (_net.isAny())
+			{
 				std::stringstream service;
 				// ... set the port only
 				service << "port=" << _port << ";";
-				announcement.addService( DiscoveryService(getDiscoveryProtocol(), service.str()));
+				announcement.addService(DiscoveryService(getDiscoveryProtocol(), service.str()));
 				return;
 			}
 
 			// do not announce if this is not our interface
-			if (iface != _net) throw dtn::net::DiscoveryBeaconHandler::NoServiceHereException();
-			
+			if (iface != _net)
+				throw dtn::net::DiscoveryBeaconHandler::NoServiceHereException();
+
 			// determine if we should enable crosslayer discovery by sending out our own address
 			bool crosslayer = dtn::daemon::Configuration::getInstance().getDiscovery().enableCrosslayer();
 
 			// this marker should set to true if we added an service description
 			bool announced = false;
 
-			try {
+			try
+			{
 				// check if cross layer discovery is disabled
-				if (!crosslayer) throw ibrcommon::Exception("crosslayer discovery disabled!");
+				if (!crosslayer)
+					throw ibrcommon::Exception("crosslayer discovery disabled!");
 
 				// get all global addresses of the interface
 				std::list<ibrcommon::vaddress> list = _net.getAddresses();
 
 				// if no address is returned... (goto catch block)
-				if (list.empty()) throw ibrcommon::Exception("no address found");
+				if (list.empty())
+					throw ibrcommon::Exception("no address found");
 
 				for (std::list<ibrcommon::vaddress>::const_iterator addr_it = list.begin(); addr_it != list.end(); ++addr_it)
 				{
 					const ibrcommon::vaddress &addr = (*addr_it);
 
 					// only announce global scope addresses
-					if (addr.scope() != ibrcommon::vaddress::SCOPE_GLOBAL) continue;
+					if (addr.scope() != ibrcommon::vaddress::SCOPE_GLOBAL)
+						continue;
 
-					try {
+					try
+					{
 						// do not announce non-IP addresses
 						sa_family_t f = addr.family();
-						if ((f != AF_INET) && (f != AF_INET6)) continue;
+						if ((f != AF_INET) && (f != AF_INET6))
+							continue;
 
 						std::stringstream service;
 						// fill in the ip address
 						service << "ip=" << addr.address() << ";port=" << _port << ";";
-						announcement.addService( DiscoveryService(getDiscoveryProtocol(), service.str()));
+						announcement.addService(DiscoveryService(getDiscoveryProtocol(), service.str()));
 
 						// set the announce mark
 						announced = true;
-					} catch (const ibrcommon::vaddress::address_exception &ex) {
+					}
+					catch (const ibrcommon::vaddress::address_exception &ex)
+					{
 						IBRCOMMON_LOGGER_DEBUG_TAG("UDPConvergenceLayer", 25) << ex.what() << IBRCOMMON_LOGGER_ENDL;
 					}
 				}
-			} catch (const ibrcommon::Exception&) {
+			}
+			catch (const ibrcommon::Exception &)
+			{
 				// address collection process aborted
 			}
 
 			// if we still not announced anything...
-			if (!announced) {
+			if (!announced)
+			{
 				// announce at least our local port
 				std::stringstream service;
 				service << "port=" << _port << ";";
-				announcement.addService( DiscoveryService(getDiscoveryProtocol(), service.str()));
+				announcement.addService(DiscoveryService(getDiscoveryProtocol(), service.str()));
 			}
 		}
 
@@ -182,7 +194,8 @@ namespace dtn
 			// get the address of the node
 			ibrcommon::vaddress addr(address, port);
 
-			try {
+			try
+			{
 				// read the bundle out of the storage
 				dtn::data::Bundle bundle = storage.get(job.getBundle());
 
@@ -195,14 +208,19 @@ namespace dtn
 				context.setBundle(bundle);
 				BundleFilter::ACTION ret = dtn::core::BundleCore::getInstance().filter(dtn::core::BundleFilter::OUTPUT, context, bundle);
 
-				switch (ret) {
-					case BundleFilter::ACCEPT:
-						break;
-					case BundleFilter::REJECT:
-					case BundleFilter::DROP:
-						dtn::net::BundleTransfer local_job = job;
-						local_job.abort(dtn::net::TransferAbortedEvent::REASON_REFUSED_BY_FILTER);
-						return;
+				switch (ret)
+				{
+				case BundleFilter::ACCEPT:
+					break;
+				case BundleFilter::REJECT:
+				case BundleFilter::DROP:
+					dtn::net::BundleTransfer local_job = job;
+					local_job.abort(dtn::net::TransferAbortedEvent::REASON_REFUSED_BY_FILTER);
+					return;
+				case BundleFilter::PASS:
+					break;
+				case BundleFilter::SKIP:
+					break;
 				}
 
 				// build the dictionary for EID lookup
@@ -212,7 +230,7 @@ namespace dtn
 				dtn::data::DefaultSerializer dummy(std::cout, dict);
 
 				// get the encoded length of the primary block
-				size_t header = dummy.getLength((const PrimaryBlock&)bundle);
+				size_t header = dummy.getLength((const PrimaryBlock &)bundle);
 				header += 20; // two times SDNV through fragmentation
 
 				dtn::data::Length size = dummy.getLength(bundle);
@@ -220,8 +238,7 @@ namespace dtn
 				if (size > m_maxmsgsize)
 				{
 					// abort transmission if fragmentation is disabled
-					if (!dtn::daemon::Configuration::getInstance().getNetwork().doFragmentation()
-							&& !bundle.get(dtn::data::PrimaryBlock::DONT_FRAGMENT))
+					if (!dtn::daemon::Configuration::getInstance().getNetwork().doFragmentation() && !bundle.get(dtn::data::PrimaryBlock::DONT_FRAGMENT))
 					{
 						throw ConnectionInterruptedException();
 					}
@@ -262,28 +279,35 @@ namespace dtn
 				// success - raise bundle event
 				dtn::net::BundleTransfer local_job = job;
 				local_job.complete();
-			} catch (const dtn::storage::NoBundleFoundException&) {
+			}
+			catch (const dtn::storage::NoBundleFoundException &)
+			{
 				// send transfer aborted event
 				dtn::net::BundleTransfer local_job = job;
 				local_job.abort(dtn::net::TransferAbortedEvent::REASON_BUNDLE_DELETED);
-			} catch (const ibrcommon::socket_exception&) {
+			}
+			catch (const ibrcommon::socket_exception &)
+			{
 				// CL is busy, requeue bundle
-			} catch (const NoAddressFoundException &ex) {
+			}
+			catch (const NoAddressFoundException &ex)
+			{
 				// no connection available
 				dtn::net::BundleTransfer local_job = job;
 				local_job.abort(dtn::net::TransferAbortedEvent::REASON_CONNECTION_DOWN);
 			}
 		}
 
-		void UDPConvergenceLayer::send(const ibrcommon::vaddress &addr, const std::string &data) noexcept (false)
+		void UDPConvergenceLayer::send(const ibrcommon::vaddress &addr, const std::string &data) noexcept(false)
 		{
 			// set write lock
 			ibrcommon::MutexLock l(m_writelock);
 
 			// get the first global scope socket
 			ibrcommon::socketset socks = _vsocket.getAll();
-			for (ibrcommon::socketset::iterator iter = socks.begin(); iter != socks.end(); ++iter) {
-				ibrcommon::udpsocket &sock = dynamic_cast<ibrcommon::udpsocket&>(**iter);
+			for (ibrcommon::socketset::iterator iter = socks.begin(); iter != socks.end(); ++iter)
+			{
+				ibrcommon::udpsocket &sock = dynamic_cast<ibrcommon::udpsocket &>(**iter);
 
 				// send converted line back to client.
 				sock.sendto(data.c_str(), data.length(), 0, addr);
@@ -299,7 +323,7 @@ namespace dtn
 			throw NoAddressFoundException("no valid address found");
 		}
 
-		void UDPConvergenceLayer::receive(dtn::data::Bundle &bundle, dtn::data::EID &sender) noexcept (false)
+		void UDPConvergenceLayer::receive(dtn::data::Bundle &bundle, dtn::data::EID &sender) noexcept(false)
 		{
 			ibrcommon::MutexLock l(m_readlock);
 
@@ -311,8 +335,9 @@ namespace dtn
 			// wait for incoming messages
 			_vsocket.select(&readfds, NULL, NULL, NULL);
 
-			if (readfds.size() > 0) {
-				ibrcommon::datagramsocket *sock = static_cast<ibrcommon::datagramsocket*>(*readfds.begin());
+			if (readfds.size() > 0)
+			{
+				ibrcommon::datagramsocket *sock = static_cast<ibrcommon::datagramsocket *>(*readfds.begin());
 
 				ibrcommon::vaddress fromaddr;
 				size_t len = sock->recvfrom(&data[0], m_maxmsgsize, 0, fromaddr);
@@ -320,7 +345,8 @@ namespace dtn
 				// add statistic data
 				_stats_in += len;
 
-				std::stringstream ss; ss << "udp://" << fromaddr.toString();
+				std::stringstream ss;
+				ss << "udp://" << fromaddr.toString();
 				sender = dtn::data::EID(ss.str());
 
 				if (len > 0)
@@ -337,74 +363,87 @@ namespace dtn
 
 		void UDPConvergenceLayer::eventNotify(const ibrcommon::LinkEvent &evt)
 		{
-			if (evt.getInterface() != _net) return;
+			if (evt.getInterface() != _net)
+				return;
 
 			switch (evt.getAction())
 			{
-				case ibrcommon::LinkEvent::ACTION_ADDRESS_ADDED:
+			case ibrcommon::LinkEvent::ACTION_ADDRESS_ADDED:
+			{
+				ibrcommon::vaddress bindaddr = evt.getAddress();
+				// convert the port into a string
+				std::stringstream ss;
+				ss << _port;
+				bindaddr.setService(ss.str());
+				ibrcommon::udpsocket *sock = new ibrcommon::udpsocket(bindaddr);
+				try
 				{
-					ibrcommon::vaddress bindaddr = evt.getAddress();
-					// convert the port into a string
-					std::stringstream ss; ss << _port;
-					bindaddr.setService(ss.str());
-					ibrcommon::udpsocket *sock = new ibrcommon::udpsocket(bindaddr);
-					try {
-						sock->up();
-						_vsocket.add(sock, evt.getInterface());
-					} catch (const ibrcommon::socket_exception&) {
-						delete sock;
-					}
-					break;
+					sock->up();
+					_vsocket.add(sock, evt.getInterface());
 				}
-
-				case ibrcommon::LinkEvent::ACTION_ADDRESS_REMOVED:
+				catch (const ibrcommon::socket_exception &)
 				{
-					ibrcommon::socketset socks = _vsocket.getAll();
-					for (ibrcommon::socketset::iterator iter = socks.begin(); iter != socks.end(); ++iter) {
-						ibrcommon::udpsocket *sock = dynamic_cast<ibrcommon::udpsocket*>(*iter);
-						if (sock->get_address().address() == evt.getAddress().address()) {
-							_vsocket.remove(sock);
-							sock->down();
-							delete sock;
-							break;
-						}
-					}
-					break;
+					delete sock;
 				}
+				break;
+			}
 
-				case ibrcommon::LinkEvent::ACTION_LINK_DOWN:
+			case ibrcommon::LinkEvent::ACTION_ADDRESS_REMOVED:
+			{
+				ibrcommon::socketset socks = _vsocket.getAll();
+				for (ibrcommon::socketset::iterator iter = socks.begin(); iter != socks.end(); ++iter)
 				{
-					ibrcommon::socketset socks = _vsocket.get(evt.getInterface());
-					for (ibrcommon::socketset::iterator iter = socks.begin(); iter != socks.end(); ++iter) {
-						ibrcommon::udpsocket *sock = dynamic_cast<ibrcommon::udpsocket*>(*iter);
+					ibrcommon::udpsocket *sock = dynamic_cast<ibrcommon::udpsocket *>(*iter);
+					if (sock->get_address().address() == evt.getAddress().address())
+					{
 						_vsocket.remove(sock);
 						sock->down();
 						delete sock;
+						break;
 					}
-					break;
 				}
+				break;
+			}
 
-				default:
-					break;
+			case ibrcommon::LinkEvent::ACTION_LINK_DOWN:
+			{
+				ibrcommon::socketset socks = _vsocket.get(evt.getInterface());
+				for (ibrcommon::socketset::iterator iter = socks.begin(); iter != socks.end(); ++iter)
+				{
+					ibrcommon::udpsocket *sock = dynamic_cast<ibrcommon::udpsocket *>(*iter);
+					_vsocket.remove(sock);
+					sock->down();
+					delete sock;
+				}
+				break;
+			}
+
+			default:
+				break;
 			}
 		}
 
 		void UDPConvergenceLayer::componentUp() noexcept
 		{
 			// routine checked for noexcept on 15.02.2013
-			try {
+			try
+			{
 				// create sockets for all addresses on the interface
 				std::list<ibrcommon::vaddress> addrs = _net.getAddresses();
 
 				// convert the port into a string
-				std::stringstream ss; ss << _port;
+				std::stringstream ss;
+				ss << _port;
 
-				for (std::list<ibrcommon::vaddress>::iterator iter = addrs.begin(); iter != addrs.end(); ++iter) {
+				for (std::list<ibrcommon::vaddress>::iterator iter = addrs.begin(); iter != addrs.end(); ++iter)
+				{
 					ibrcommon::vaddress &addr = (*iter);
 
-					try {
+					try
+					{
 						// handle the addresses according to their family
-						switch (addr.family()) {
+						switch (addr.family())
+						{
 						case AF_INET:
 						case AF_INET6:
 							addr.setService(ss.str());
@@ -413,7 +452,9 @@ namespace dtn
 						default:
 							break;
 						}
-					} catch (const ibrcommon::vaddress::address_exception &ex) {
+					}
+					catch (const ibrcommon::vaddress::address_exception &ex)
+					{
 						IBRCOMMON_LOGGER_TAG("UDPConvergenceLayer", warning) << ex.what() << IBRCOMMON_LOGGER_ENDL;
 					}
 				}
@@ -425,7 +466,9 @@ namespace dtn
 
 				// register as discovery handler for this interface
 				dtn::core::BundleCore::getInstance().getDiscoveryAgent().registerService(_net, this);
-			} catch (const ibrcommon::socket_exception &ex) {
+			}
+			catch (const ibrcommon::socket_exception &ex)
+			{
 				IBRCOMMON_LOGGER_TAG("UDPConvergenceLayer", error) << "bind failed (" << ex.what() << ")" << IBRCOMMON_LOGGER_ENDL;
 			}
 		}
@@ -453,7 +496,8 @@ namespace dtn
 
 			while (_running)
 			{
-				try {
+				try
+				{
 					dtn::data::Bundle bundle;
 					EID sender;
 
@@ -463,20 +507,28 @@ namespace dtn
 					context.setBundle(bundle);
 					BundleFilter::ACTION ret = dtn::core::BundleCore::getInstance().filter(dtn::core::BundleFilter::INPUT, context, bundle);
 
-					switch (ret) {
-						case BundleFilter::ACCEPT:
-							// raise default bundle received event
-							dtn::net::BundleReceivedEvent::raise(sender, bundle, false);
-							break;
-
-						case BundleFilter::REJECT:
-						case BundleFilter::DROP:
-							break;
+					switch (ret)
+					{
+					case BundleFilter::ACCEPT:
+						// raise default bundle received event
+						dtn::net::BundleReceivedEvent::raise(sender, bundle, false);
+						break;
+					case BundleFilter::REJECT:
+						break;
+					case BundleFilter::DROP:
+						break;
+					case BundleFilter::PASS:
+						break;
+					case BundleFilter::SKIP:
+						break;
 					}
-
-				} catch (const dtn::InvalidDataException &ex) {
+				}
+				catch (const dtn::InvalidDataException &ex)
+				{
 					IBRCOMMON_LOGGER_DEBUG_TAG("UDPConvergenceLayer", 2) << "Received a invalid bundle: " << ex.what() << IBRCOMMON_LOGGER_ENDL;
-				} catch (const std::exception&) {
+				}
+				catch (const std::exception &)
+				{
 					return;
 				}
 				yield();
@@ -493,5 +545,5 @@ namespace dtn
 		{
 			return "UDPConvergenceLayer";
 		}
-	}
-}
+	} // namespace net
+} // namespace dtn
